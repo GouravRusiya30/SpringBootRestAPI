efb5cca19f914dd7e4182383621e8f060c744c45
Gourav Rusiya <gouravrusiya.lnct@gmail.com>
2019-01-08 11:43:22 +0530

    codecov added

--- src/main/java/com/gourav/restApi/RestApiApplication.java
+++ src/main/java/com/gourav/restApi/RestApiApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestTutorialApplication {
+public class RestApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RestTutorialApplication.class, args);
+		SpringApplication.run(RestApiApplication.class, args);
 	}
 }
