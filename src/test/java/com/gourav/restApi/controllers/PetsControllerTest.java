8e5cb2b6b637bed202b53d648343310283a9db54
Gourav Rusiya <gouravrusiya.lnct@gmail.com>
2019-01-08 23:04:54 +0530

    removed this test

--- src/test/java/com/gourav/restApi/controllers/PetsControllerTest.java
+++ src/test/java/com/gourav/restApi/controllers/PetsControllerTest.java
@@ -1,20 +1,20 @@
-package com.gourav.restApi.controllers;
-
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-
-@RunWith(SpringRunner.class)
-@WebMvcTest(value = PetsController.class)
-public class PetsControllerTest {
-	
-	@Autowired
-	private MockMvc mvc;
-
-	@MockBean
-	private PetsController petsController;
-	
-}
+//package com.gourav.restApi.controllers;
+//
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+//import org.springframework.boot.test.mock.mockito.MockBean;
+//import org.springframework.test.context.junit4.SpringRunner;
+//import org.springframework.test.web.servlet.MockMvc;
+//
+//@RunWith(SpringRunner.class)
+//@WebMvcTest(value = PetsController.class)
+//public class PetsControllerTest {
+//	
+//	@Autowired
+//	private MockMvc mvc;
+//
+//	@MockBean
+//	private PetsController petsController;
+//	
+//}
