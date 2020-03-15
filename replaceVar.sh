757a9ce562740e080ef462c829e85d682edb8054
Gourav Rusiya <gouravrusiya.lnct@gmail.com>
2020-03-15 14:21:14 +0530

    test

--- replaceVar.sh
+++ replaceVar.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+echo "replacing secrets from travis secure variables"
 sed -i "" -e "s|%MONGO_PWD%|MONGO_PWD|g" src/main/resources/application.properties
+
+actual_value=MONGO_PWD
+
+echo $actual_value
